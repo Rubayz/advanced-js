6ed2d16630b3185446b6f140849a2631e0af5b59
Rubayz <rubayzaf2007@gmail.com>
2020-08-08 15:59:02 +0600

    null and undefined finished for interview

---
+++ null-vs-understand.js
@@ -0,0 +1,18 @@
+// let pakhi;
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return;
+// }
+
+// function add(num1, num2=10){
+//     console.log(num1, num2);
+// }
+
+// const premik = {name:"Smart Dude", phone:017733773}
+// console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 11, 45];
+console.log(ages[5]);
