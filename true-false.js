95b20b22924889b42f782e52a05f69bfb1a53085
Rubayz <rubayzaf2007@gmail.com>
2020-08-08 14:30:18 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,14 @@
+//fALSY
+//0
+//""
+//undefined and null
+//Nan
+//Truthy
+//'0', ' ', [ ], { }
+let name = 12;
+if (name || name == 0) {
+    console.log("Condition is true.");
+}
+else{
+    console.log("Condition is false.");
+}
