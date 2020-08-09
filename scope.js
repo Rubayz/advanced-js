60ce802b91d562cacf850773829cdd123d4caea6
Rubayz <rubayzaf2007@gmail.com>
2020-08-09 16:23:25 +0600

    i don't fall in scope

---
+++ scope.js
@@ -0,0 +1,18 @@
+function sum(first, second){
+    let result = first + second;
+    // console.log(bonus);
+    if(result > 9){
+        let mood = "happy";
+        mood = "fishy";
+        mood = "cripy";
+        mood = "funky";
+        // console.log(mood);
+    }
+    console.log(day);
+    const day = "friday";
+    return result;
+}
+
+const output = sum(3, 7);
+// console.log(bonus);
+// console.log(output);
